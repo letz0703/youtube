8d05d2589597d70d7bb6694e1a6012cfab451f1e
ManI <letz0703@gmail.com>
2022-06-08 22:41:24 +0900

    enforcing code style
    Eslint

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+import "./App.css";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/tachyons@4.12.0/css/tachyons.min.css"
+        />
+      </div>
+    );
+  }
+}
