de4b75be5de59a7c98dcbe4655ff53bbe20e0624
ManI <letz0703@gmail.com>
2022-06-08 22:41:24 +0900

    enforcing code style

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <link
+        rel="stylesheet"
+        href="https://unpkg.com/tachyons@4.12.0/css/tachyons.min.css"
+      />
+      <h1 className="tc">working!!</h1>;
+    </>
+  );
+}
+
+export default App;
