b961bfe4c5e46c3e942e40c42778af230dfb0469
ManI <letz0703@gmail.com>
2022-06-09 13:55:07 +0900

    ytlist.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,22 @@ import "./App.css";
 import YtList from "./components/ytlist";
 
 export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      links: [{ name: "letz" }, { name: "rainskiss" }],
+    };
+  }
+
   render() {
+    const { links } = this.state;
     return (
       <div>
         <link
           rel="stylesheet"
           href="https://unpkg.com/tachyons@4.12.0/css/tachyons.min.css"
         />
-        <YtList />
+        <YtList links={links} />
       </div>
     );
   }
