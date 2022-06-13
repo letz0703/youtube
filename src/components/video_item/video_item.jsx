43860d528294a1735f67fb044f17c8a9e1859fe4
ManI <letz0703@gmail.com>
2022-06-13 17:30:46 +0900

    --no-verify
    220613 wip: video_item scss

--- src/components/video_item/video_item.jsx
+++ src/components/video_item/video_item.jsx
@@ -1,10 +1,18 @@
 import React, { memo } from "react";
-
+import styles from "../video_item/video_item.module.css";
 const VideoItem = ({ video: { snippet } }) => (
-  <li>
-    <img src={snippet.thumbnails.medium.url} alt="video tumbnail" />
-    <p>{snippet.title}</p>
-    <p className="f4">{snippet.description}</p>
+  <li className={styles.container}>
+    <div className={styles.video}>
+      <img
+        src={snippet.thumbnails.medium.url}
+        alt="video tumbnail"
+        className={styles.thumb}
+      />
+      <div className={styles.metadata}>
+        <p className={styles.title}>{snippet.title}</p>
+        <p className={snippet.channel}>{snippet.channelTitle}</p>
+      </div>
+    </div>
   </li>
 );
 
--- src/components/video_list/video_list.jsx
+++ src/components/video_list/video_list.jsx
@@ -1,8 +1,9 @@
 import VideoItem from "../video_item/video_item";
+import styles from "../video_list/video_list.module.css";
 
 const VideoList = (props) =>
   props.videos.map((video) => (
-    <ul>
+    <ul className={styles.videos}>
       <VideoItem key={video.id} video={video} />
     </ul>
   ));
