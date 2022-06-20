9cb61d2d04d534c2dbbd67896efa4a6a35067996
ManI <letz0703@gmail.com>
2022-06-20 22:10:14 +0900

    REACT_APP_YOUTUBE_API_KEY

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import Youtube from "./services/youtube";
 
-const youtube = new Youtube("kdjfkd");
+const youtube = new Youtube(process.env.REACT_APP_YOUTUBE_API_KEY);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
